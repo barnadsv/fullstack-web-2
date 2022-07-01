59a6d109c04d5b0a1b3c57c104737d3ece7ae89b
barnadsv <barnadsv@gmail.com>
2022-07-01 07:29:08 +0000

    Add PATCH endpoint - update text

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -5,3 +5,12 @@ import { api } from './_api'
 export const del = (event: RequestEvent) => {
     return api(event)
 }
+
+/** @type {import('./__types/items').RequestHandler} */
+export async function patch(event: RequestEvent) {
+    const formData = await event.request.formData()
+    const texto = formData.get('text')?.toString()
+    return api(event, {
+        text: texto
+    })
+}
--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -3,7 +3,7 @@ import type { RequestEvent } from "@sveltejs/kit"
 // TODO: Persist in database
 let todos: Todo[] = []
 
-export const api = (event: RequestEvent, todo?: Todo) => {
+export const api = (event: RequestEvent, data?: Record<string, unknown>) => {
     let body = {}
     let status = 500
     switch (event.request.method.toUpperCase()) {
@@ -12,14 +12,25 @@ export const api = (event: RequestEvent, todo?: Todo) => {
             status = 200
             break
         case 'POST':
-            if (todo) {
-                todos.push(todo)
-                body = todo
+            if (data) {
+                todos.push(data as Todo)
+                body = data
                 status = 201
             }
             break
         case 'DELETE':
             todos = todos.filter(todo => todo.uid !== event.params.uid)
+            status = 200
+            break
+        case 'PATCH':
+            if (data) {
+                todos = todos.map(todo => {
+                    if (todo.uid === event.params.uid) {
+                        todo.text = data.text as string
+                    }
+                    return todo
+                })
+            }
             status = 200
             break
         default:
